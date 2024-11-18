937b734f8730e84d191ad5547c2625e81d678105
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2024-11-18 19:24:15 -0300

    setting `wasLoggedToday` field when fetching habits

--- server/src/modules/habits/habits.module.ts
+++ server/src/modules/habits/habits.module.ts
@@ -4,9 +4,10 @@ import { User } from '../users/entities/user.entity';
 import { Habit } from './entities/habit.entity';
 import { HabitsService } from './services/habits.service';
 import { HabitsController } from './controllers/habits.controller';
+import { HabitLog } from '../habit-logs/entities/habit-log.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Habit, User])],
+  imports: [TypeOrmModule.forFeature([Habit, HabitLog, User])],
   exports: [TypeOrmModule],
   providers: [HabitsService],
   controllers: [HabitsController],
--- server/src/modules/habits/services/habits.service.ts
+++ server/src/modules/habits/services/habits.service.ts
@@ -11,33 +11,55 @@ import { User } from 'src/modules/users/entities/user.entity';
 import { UpdateHabitDto } from '../dtos/update-habit.dto';
 import { ReadHabitSummaryDto } from '../dtos/read-habit-summary.dto';
 import { ReadHabitDto } from '../dtos/read-habit.dto';
+import { HabitLog } from 'src/modules/habit-logs/entities/habit-log.entity';
 
 @Injectable()
 export class HabitsService {
   constructor(
     @InjectRepository(Habit)
     private readonly habitsRepository: Repository<Habit>,
+    @InjectRepository(HabitLog)
+    private readonly habitLogsRepository: Repository<HabitLog>,
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
   ) {}
 
   async getHabits(userId: string): Promise<ReadHabitSummaryDto[]> {
-    const habits = await this.habitsRepository.find({
-      where: { user: { id: userId }, isDeleted: false },
-    });
+    const today = new Date();
+    today.setUTCHours(0, 0, 0, 0);
+
+    const habits = await this.habitsRepository
+      .createQueryBuilder('habit')
+      .leftJoinAndSelect('habit.logs', 'habitLog', 'habitLog.date = :today', {
+        today,
+      })
+      .where('habit.userId = :userId', { userId })
+      .andWhere('habit.isDeleted = false')
+      .getMany();
+
+    console.log(habits);
 
     return habits.map((habit) => ({
       id: habit.id,
       name: habit.name,
-      wasLoggedToday: false, // TODO: update after implementing Habit Log
+      wasLoggedToday: habit.logs.length > 0,
       wasValidatedToday: false, // TODO: update after implementing Habit Log Validation
     }));
   }
 
   async getHabit(userId: string, habitId: string): Promise<ReadHabitDto> {
-    const habit = await this.habitsRepository.findOne({
-      where: { id: habitId, user: { id: userId }, isDeleted: false },
-    });
+    const today = new Date();
+    today.setUTCHours(0, 0, 0, 0);
+
+    const habit = await this.habitsRepository
+      .createQueryBuilder('habit')
+      .leftJoinAndSelect('habit.logs', 'habitLog', 'habitLog.date = :today', {
+        today,
+      })
+      .where('habit.id = :habitId', { habitId })
+      .andWhere('habit.userId = :userId', { userId })
+      .andWhere('habit.isDeleted = false')
+      .getOne();
 
     if (!habit) {
       throw new NotFoundException(`Habit with Id ${habitId} not found.`);
@@ -47,8 +69,8 @@ export class HabitsService {
       id: habit.id,
       name: habit.name,
       rules: habit.rules,
-      wasLoggedToday: false, // TODO: update after implementing Habit Log
-      wasValidatedToday: false, // TODO: update after implementing Habit Log
+      wasLoggedToday: habit.logs.length > 0,
+      wasValidatedToday: false, // TODO: update after implementing Habit Log Validation
       currentStreak: 0, // TODO: update after implementing streaks
       highestStreak: 0, // TODO: update after implementing streaks
     };
